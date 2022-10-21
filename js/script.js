2b78e376fc77c87a2cb22d4de56c3910ee3d99e3
Nelson ANDRADE <nelsonandrade7771@gmail.com>
2022-10-21 14:58:55 +0200

    update @media

--- js/script.js
+++ js/script.js
@@ -8,4 +8,13 @@ menu.onclick = () => {
 window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('active');
-}
+};
+
+const sr = ScrollReveal ({
+    distance:'40px',
+    duration: 2600,
+    reset: true
+})
+
+sr.reveal('.home-text', {delay:300, origin:'left'})
+sr.reveal('.about, .services, .portfolio, .blog, .contact,.copyright',{delay:100, origin:'bottom'})
