34f811456eaeb637ba989af331b1638d6aa64733
Nelson ANDRADE <nelsonandrade7771@gmail.com>
2022-12-15 16:19:33 +0100

    nouvelles photos

--- js/script.js
+++ js/script.js
@@ -60,3 +60,26 @@ $(function() {
         $(".about-text").text("Sobre Mim");
     });
 });
+
+/*dark mode */
+const btnToggle = document.querySelector('.btn-toggle');
+
+btnToggle.addEventListener('click', () => {
+
+    const body = document.body;
+
+    if(body.classList.contains('dark')){
+
+        body.classList.add('light')
+        body.classList.remove('dark')
+        btnToggle.innerHTML = "Dark"
+
+    } else if(body.classList.contains('light')){
+
+        body.classList.add('dark')
+        body.classList.remove('light')
+        btnToggle.innerHTML = "Light"
+
+    }
+
+});
