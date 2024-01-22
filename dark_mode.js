7547093f828818264d52fb3f6b8dffa7967d28cc
Nelson ANDRADE <nelsonandrade7771@gmail.com>
2024-01-22 19:54:12 +0100

    update insta

--- dark_mode.js
+++ dark_mode.js
@@ -8,5 +8,5 @@ const changeThemeToDark = () => {
     localStorage.setItem("data-theme", 'light');
     }
    let theme = localStorage.getItem('data-theme');
-   if (theme == 'dark') changeThemeToDark();//
+   if (theme == 'dark') changeThemeToDark();
     
